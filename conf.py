57fbde1bd7c51fb5db8da0f1a14e4f96b7f1f8ba
Aishwarya Mishra <aishwarya@robustest.com>
2020-08-09 04:09:39 +0530

    logo

--- conf.py
+++ conf.py
@@ -83,9 +83,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "html_logo": "logo.png"
-}
+#html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -117,6 +115,7 @@ def setup(app):
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'RobusTest20Documentationdoc'
 
+html_logo = 'logo.png'
 
 # -- Options for LaTeX output ------------------------------------------------
 
